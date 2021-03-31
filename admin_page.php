63ee58619d68b7659d76febaa8f864082676d5f4
peabody28 <onmmanmn@gmail.com>
2021-03-31 14:05:06 +0300

    unst

--- admin_page.php
+++ admin_page.php
@@ -15,7 +15,6 @@ else if($_GET["code"]=="change_course")
 }
 else
 {
-
     $content = "<br><br><div class='row col-12 p-0 m-0 ml-5'><a class='btn create' href='/admin_page.php?code=create_course'>Cоздать курс</a> </div><br><br>";
     $courses = new Course();
     $courses_list = $courses->get_courses();
@@ -33,19 +32,6 @@ $page->argv = ['title' => "main",
     'css' => "/css/admin_page.css",
     "name" => "<h2>$_SESSION[name] - admin-tools</h2>",
     "content" => $content,
-    "js" => "/js/admin_page.js",
-
-    'js2'=>"<script src='https://cdn.tiny.cloud/1/3yb9s681223ydg8xq85axmko4fjf3485tan985upzgql2yqm/tinymce/5/tinymce.min.js' referrerpolicy='origin'></script>",
-    "textov"=>"<script>
-tinymce.init({
-    selector: 'textarea',
-    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-    toolbar_mode: 'floating',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
-});
-</script>"
-    ];
+    "js" => "/js/admin_page.js"];
 echo $page->render_page();
 
