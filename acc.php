e7fb85c790c055ade69253e22c97734dafd9a1fb
peabody28 <onmmanmn@gmail.com>
2021-06-24 18:23:02 +0300

    Файлы css и 404

--- acc.php
+++ acc.php
@@ -7,7 +7,7 @@ $content = "";
 $l = $_SESSION["rights"];
 if($_SESSION["rights"]=="admin")
     $content.= "<div class='row w-100 p-2 m-0 justify-content-center'><a class='btn btn-primary' href='/admin_page.php'>Админка</a></div>";
-
+$content.= "<div class='row w-100 p-2 m-0 justify-content-center'><a id='exit' class='btn btn-md' href='/exit.php'>Выйти</a></div>";
 $file = basename(__FILE__, ".php");
 
 $page = new Render();
